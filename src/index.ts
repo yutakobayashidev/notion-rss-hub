a8a51bdde9615dc7b47b4e78bfcddea1ecc5167e
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-02-25 23:32:31 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ async function updateArticlePage(pageId: string, article: Article) {
 
 // TODO: キーを変更可能にする
 function articleProperties(article: Article): InputPropertyValueMap {
-  const hostname = "Post on" + article.Url
+  const hostname = "Posted" + article.Url
   ? getHostFromURL(article.Url)
   : null;
   return {
@@ -127,7 +127,9 @@ function articleProperties(article: Article): InputPropertyValueMap {
     },
     Action: {
       "type": "text",
-      "text": hostname,
+      "text": {
+        "content": hostname
+    },
     },
     from_rss: {
       type: 'checkbox',
