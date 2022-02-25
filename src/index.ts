a4b06f95be4b45d7bce92109ff9bf50f29bb2b67
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-02-26 00:06:51 +0900

    fix: rich_text

--- src/index.ts
+++ src/index.ts
@@ -122,7 +122,7 @@ function articleProperties(article: Article): InputPropertyValueMap {
       ],
     },
     Action: {
-      type: 'text',
+      type: 'rich_text',
       text: {
         content: hostname
       },
